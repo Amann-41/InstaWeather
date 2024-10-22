3ddfdd6a02c08754a56256652c322cb532b1c2c9
amannn11111 <aman14sen@gmail.com>
2024-10-22 19:59:39 +0530

    Initial commit

---
+++ InstaWeather/src/test/java/com/example/InstaWeather/InstaWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InstaWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# InstaWeather
+ A web app that delivers instant weather updates for any location using Spring Boot and the OpenWeatherMap API.
